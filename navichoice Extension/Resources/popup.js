d15591f5881e92a41a5d08b499399f2ee1ee1253
Jacob Ridgwell <ridgwell@byu.edu>
2021-10-14 07:45:23 -0700

    final 1.0 version

--- navichoice Extension/Resources/popup.js
+++ navichoice Extension/Resources/popup.js
@@ -1,11 +1,3 @@
-//console.log("Hello World from popup js", browser);
-//
-//browser.runtime.sendMessage({ greeting: "hello greeting from popup js" }).then((response) => {
-//    console.log("Received response in popupjs: ", response);
-//});
-//
-//document.querySelector("p").innerText = 'updated via javascript';
-
 const defaultNaviPromise = browser.storage.local.get("defaultNavi");
 const btns = document.querySelectorAll("input");
 
@@ -29,27 +21,9 @@ btns[1].addEventListener("click", navigationChoiceWaze);
 async function navigationChoiceApple(event) {
 	const err = await browser.storage.local.set({ defaultNavi: "apple-maps" });
 	if (err) console.log(`error: ${err}`);
-	navigationChosen("apple");
 }
 
 async function navigationChoiceWaze(event) {
 	const err = await browser.storage.local.set({ defaultNavi: "waze" });
 	if (err) console.log(`error: ${err}`);
-	navigationChosen("waze");
-}
-
-function navigationChosen(name) {
-	// browser.runtime.sendMessage({ name }).then((response) => {
-	// 	console.log("Received response: ", response);
-	// });
-	// browser.runtime.sendNativeMessage(
-	// 	"application.id",
-	// 	{ message: name },
-	// 	function (response) {
-	// 		if (response) {
-	// 			console.log("Received sendNativeMessage response:");
-	// 			console.log(response);
-	// 		}
-	// 	}
-	// );
 }
