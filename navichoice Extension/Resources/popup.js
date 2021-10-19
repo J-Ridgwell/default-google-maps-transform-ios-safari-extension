a130444f1fb2816a7decb9b4809543b46e4884ba
Jacob Ridgwell <ridgwell@byu.edu>
2021-10-19 15:22:26 -0700

    updated popup

--- navichoice Extension/Resources/popup.js
+++ navichoice Extension/Resources/popup.js
@@ -1,29 +1,36 @@
 const defaultNaviPromise = browser.storage.local.get("defaultNavi");
-const btns = document.querySelectorAll("input");
+const btns = document.querySelectorAll("button");
+const outlineCSS = "3px solid #78c5ef";
 
 defaultNaviPromise.then((items) => {
-	switch (items.defaultNavi) {
-		case "waze":
-			btns[1].checked = true;
-			break;
-		case "apple-maps":
-			btns[0].checked = true;
-			break;
-		default:
-			browser.storage.local.set({ defaultNavi: "apple-maps" });
-			btns[0].checked = true;
+	if (items.defaultNavi === "waze" || items.defaultNavi === "apple-maps") {
+		addOutline(items.defaultNavi);
+	} else {
+		browser.storage.local.set({ defaultNavi: "apple-maps" });
+		addOutline("apple-maps");
 	}
 });
 
-btns[0].addEventListener("click", navigationChoiceApple);
-btns[1].addEventListener("click", navigationChoiceWaze);
+btns[0].addEventListener("click", navigationChoice);
+btns[1].addEventListener("click", navigationChoice);
 
-async function navigationChoiceApple(event) {
-	const err = await browser.storage.local.set({ defaultNavi: "apple-maps" });
-	if (err) console.log(`error: ${err}`);
+async function navigationChoice(event) {
+	const err = await browser.storage.local.set({ defaultNavi: this.id ?? "apple-maps" });
+
+	if (err) {
+		console.log(`error: ${err}`);
+		addOutline("apple-maps");
+	} else {
+		addOutline(this.id);
+	}
 }
 
-async function navigationChoiceWaze(event) {
-	const err = await browser.storage.local.set({ defaultNavi: "waze" });
-	if (err) console.log(`error: ${err}`);
+function addOutline(appleOrWaze) {
+	if (appleOrWaze === "apple-maps") {
+		btns[1].style.border = outlineCSS;
+		btns[0].style.border = "";
+	} else {
+		btns[0].style.border = outlineCSS;
+		btns[1].style.border = "";
+	}
 }
