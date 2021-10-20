65b889c03bc36ec523a9755763c1496126df8fac
Jacob Ridgwell <ridgwell@byu.edu>
2021-10-20 16:50:37 -0700

    add toggle for removing google search ads

--- navichoice Extension/Resources/content.js
+++ navichoice Extension/Resources/content.js
@@ -1,5 +1,5 @@
 const defaultNaviPromise = browser.storage.local.get("defaultNavi");
-
+const removeGoogleSearchAdsPromise = browser.storage.local.get("removeGoogleSearchAds");
 const querySelectorString =
 	'[data-url^="https://maps.google.com"], [href^="https://maps.google.com"], [data-link^="https://maps.google.com"]';
 
@@ -18,7 +18,13 @@ const observer = new MutationObserver(() => {
 });
 
 window.addEventListener("load", (event) => {
-    removeAds();
+	removeGoogleSearchAdsPromise.then((items) => {
+		console.log(`promise has resolved. remove ads= ${items.removeGoogleSearchAds}`);
+		if (items.removeGoogleSearchAds) {
+			console.log("in if");
+			removeAds();
+		}
+	});
 	defaultNaviPromise.then(
 		(items) => {
 			authority = items.defaultNavi === "waze" ? wazeAuthority : appleMapsAuthority;
@@ -43,15 +49,18 @@ function findElements() {
 				: "href";
 
 			const attributeValue = elem.getAttribute(attributeKey);
-            const extractedAddress = extractAddress(elem, attributeValue);
+			const extractedAddress = extractAddress(elem, attributeValue);
 			const convertedURL = createNavigationURL(extractedAddress);
-            
+
 			elem.setAttribute(attributeKey, convertedURL ?? attributeValue);
 		}
 	} catch (e) {
 		console.error("error in findElements(): " + e.toString());
 	}
 }
+/*
+<a href="https://maps.google.com/maps?client=safari&amp;hl=en&amp;biw=390&amp;bih=664&amp;gs_lcp=ChNtb2JpbGUtZ3dzLXdpei1zZXJwEAEYBDICCCkyAggpMgQIKRBHMgIIKTICCCkyAggpMgIIKTICCCkyAggpMgIIKTICCCkyAggpMgIIKTICCCkyAggpMg4IABDqAhCPARCMAxDlAjIOCAAQ6gIQjwEQjAMQ5QIyDggAEOoCEI8BEIwDEOUCMg4IABDqAhCPARCMAxDlAjIOCAAQ6gIQjwEQjAMQ5QIyDggAEOoCEI8BEIwDEOUCMg4IABDqAhCPARCMAxDlAjIOCAAQ6gIQjwEQjAMQ5QIyDggAEOoCEI8BEIwDEOUCMg4IABDqAhCPARCMAxDlAjIOCAAQ6gIQjwEQjAMQ5QIyDggAEOoCEI8BEIwDEOUCMg4IABDqAhCPARCMAxDlAjIOCAAQ6gIQjwEQjAMQ5QIyDggAEOoCEI8BEIwDEOUCUABYAGDfEGgCcAB4AIABAIgBAJIBAJgBAKABAbABHsABAQ&amp;um=1&amp;ie=UTF-8&amp;daddr=4250+Rosewood+Dr,+Pleasanton,+CA+94588&amp;geocode=FfE9PwIdVU28-CnvlTmQN-mPgDF7o6SInLeCGg&amp;ei=OKJwYeLfBsnP0PEPrbCioAo" data-tu="//www.googleadservices.com/pagead/aclk?sa=L&amp;ai=DChcSEwjcuZu6jtrzAhX_Da0GHVprAuMYABAFGgJwdg&amp;ohost=www.google.com&amp;cid=CAASEuRog2Ibk81pZa0knWh5e4Tqwg&amp;sig=AOD64_0lZLaeSCOuJbAzusGB3efp_Zl1JA&amp;q&amp;ctype=11&amp;adurl" onclick="wlctc(this); return false;" data-ved="2ahUKEwji25K6jtrzAhXJJzQIHS2YCKQQi0F6BAgEEAk"><span class="WzmYQb S003Ke ZoN4Lb z1asCe QoWzUe"><svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71 11.29l-9-9c-.39-.39-1.02-.39-1.41 0l-9 9c-.39.39-.39 1.02 0 1.41l9 9c.39.39 1.02.39 1.41 0l9-9c.39-.38.39-1.01 0-1.41zM14 14.5V12h-4v3H8v-4c0-.55.45-1 1-1h5V7.5l3.5 3.5-3.5 3.5z"></path></svg></span></a>
+*/
 
 // potentially still need to take care of character encodings for '+' or '&' in the name of the address
 function extractAddress(elem, attributeValue) {
@@ -59,7 +68,7 @@ function extractAddress(elem, attributeValue) {
 	const stringToFind = "https://maps.google.com/maps/dir//";
 	const stringToFind1 = "https://maps.google.com/maps/place//data=";
 	const stringToFind2 = "https://maps.google.com/maps?q=";
-    const stringToFind3 = "https://maps.google.com/maps/place/";
+	const stringToFind3 = "https://maps.google.com/maps/place/";
 	let address;
 
 	try {
@@ -71,16 +80,16 @@ function extractAddress(elem, attributeValue) {
 		}
 		if (url.includes(stringToFind1)) {
 			address = elem.innerText.replaceAll(" ", "+").replaceAll(",", "");
-            urlParams.set("beforeAddress", "?daddr=");
-        } else if (url.includes(stringToFind3)) {
-            address = url
-                .slice(stringToFind3.length, url.indexOf("/", stringToFind3.length + 1))
-                .replaceAll(",", "");
-            urlParams.set("beforeAddress", "?daddr=");
-        }
+			urlParams.set("beforeAddress", "?daddr=");
+		} else if (url.includes(stringToFind3)) {
+			address = url
+				.slice(stringToFind3.length, url.indexOf("/", stringToFind3.length + 1))
+				.replaceAll(",", "");
+			urlParams.set("beforeAddress", "?daddr=");
+		}
 		if (url.includes(stringToFind2)) {
 			address = url.slice(stringToFind2.length, url.indexOf("&"));
-            urlParams.set("beforeAddress", "?q=");
+			urlParams.set("beforeAddress", "?q=");
 		}
 	} catch (e) {
 		console.error(`Error in string manipulation: ${e.toString()}`);
@@ -92,7 +101,7 @@ function extractAddress(elem, attributeValue) {
 		urlParams.set("beforeAddress", "?q=");
 		urlParams.set("afterAddress", "&navigation=yes");
 	}
-    
+
 	return address;
 }
 
@@ -104,12 +113,23 @@ function createNavigationURL(address) {
 }
 
 function removeAds() {
-    const elems = document.querySelectorAll("data-text-ad=1");
-    for (const elem of elems) {
-        elem.remove();
-    }
-
+	console.log("in remove ads");
+	try {
+		const elems = document.querySelectorAll("[data-text-ad='1']");
+		console.log(`Found ${elems.length} ads to be removed`);
+		for (const elem of elems) {
+			elem.remove();
+		}
+	} catch (e) {
+		console.error(`Error search for ads: ${e.toString()}`);
+	}
 }
+
+// let addressPatterns = new Map();
+// addressPatterns.set("https://maps.google.com/maps/dir//", () => {
+// 	address = url.slice(stringToFind.length, url.indexOf("/data=")).replaceAll(",", "");
+// 	urlParams.set("beforeAddress", "?daddr=");
+// });
 
 //console.log("url = " + window.location.href);
 //console.log("webNav = " + window.WebNavigation);
--- navichoice Extension/Resources/popup.js
+++ navichoice Extension/Resources/popup.js
@@ -1,6 +1,14 @@
 const defaultNaviPromise = browser.storage.local.get("defaultNavi");
+const removeAdsPromise = browser.storage.local.get("removeGoogleSearchAds");
 const btns = document.querySelectorAll("button");
+const removeAdsCheckbox = document.getElementById("remove-ads-checkbox");
 const outlineCSS = "3px solid #78c5ef";
+
+removeAdsPromise.then((items) => {
+	if (items.removeGoogleSearchAds) {
+		removeAdsCheckbox.checked = true;
+	}
+});
 
 defaultNaviPromise.then((items) => {
 	if (items.defaultNavi === "waze" || items.defaultNavi === "apple-maps") {
@@ -13,7 +21,10 @@ defaultNaviPromise.then((items) => {
 
 btns[0].addEventListener("click", navigationChoice);
 btns[1].addEventListener("click", navigationChoice);
-
+removeAdsCheckbox.addEventListener("click", () => {
+    console.log(removeAdsCheckbox.checked)
+	browser.storage.local.set({ removeGoogleSearchAds: removeAdsCheckbox.checked });
+});
 async function navigationChoice(event) {
 	const err = await browser.storage.local.set({ defaultNavi: this.id ?? "apple-maps" });
 
@@ -28,9 +39,13 @@ async function navigationChoice(event) {
 function addOutline(appleOrWaze) {
 	if (appleOrWaze === "apple-maps") {
 		btns[1].style.border = outlineCSS;
+		btns[1].style.fontWeight = "bold";
 		btns[0].style.border = "";
+		btns[0].style.fontWeight = "normal";
 	} else {
 		btns[0].style.border = outlineCSS;
+		btns[0].style.fontWeight = "bold";
 		btns[1].style.border = "";
+		btns[1].style.fontWeight = "normal";
 	}
 }
